b1847700a07ecb9fb73fc44ca17c67beffe5100d
Angel Arcoraci <angel.arcoraci@gmail.com>
2020-08-23 15:38:05 -0300

    small info on centralized api

--- utils/api.js
+++ utils/api.js
@@ -1,3 +1,9 @@
+/**
+ * more info and ideas on how to centralize api and access store
+ * https://stackoverflow.com/questions/40151178/attach-custom-headers-before-every-fetch-request-in-react-native
+ * https://daveceddia.com/access-redux-store-outside-react/#option-1-export-the-store
+ */
+
 import fetch from 'cross-fetch';
 // import store from '../state';
 
